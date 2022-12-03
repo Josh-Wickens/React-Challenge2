e0265b31daeaeabc38567a0dd5052436f558955c
Josh-Wickens <joshwickens@hotmail.com>
2022-12-03 12:32:12 +0000

    create simple navbar using react

---
+++ my-app/src/components/NavBarSimple.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import css from "./css/NavBarSimple.module.css"
+
+class NavBarSimple extends Component {
+
+    constructor(props) {
+      super(props);
+    
+      this.state = {
+         message: "Hello, Guest!",
+         buttonText: "Log in"
+      }
+    }
+
+    handleClick() {
+        this.setState((prevState, prevProps) => {
+            return {
+                message:
+                prevState.message === "Hello, Guest!" ? "Welcome back User!" : "Hello, Guest!",
+                buttonText:
+                prevState.buttonText === "Log out" ? "Log in" : "Log out",
+
+            };
+        });
+    }
+  render() {
+    return (
+      <div className= {css.NavBar}>
+        <h1>My Gallery</h1>
+        <div>
+            <span> {this.state.message}</span>
+            <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default NavBarSimple
