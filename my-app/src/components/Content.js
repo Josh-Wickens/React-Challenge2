7e1f4c8ebdebe57d41dabf6598842001b6d2482f
Josh-Wickens <joshwickens@hotmail.com>
2022-12-03 20:31:38 +0000

    rendering a list of objects

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { savedPosts } from "../posts.json";
 import css from "./css/Content.module.css"
+import PostItem from './PostItem';
 
 
 export class Content extends Component {
@@ -13,18 +14,13 @@ export class Content extends Component {
             <div className={css.Content}>
                 <div className={css.TitleBar}><h1>My Photos</h1></div>
                 <div className={css.SearchResults}>
-                    {savedPosts.map((post) => {
-                    return <div className={css.SearchItem} key={post.title}>
-                        <p>{post.title}</p>
-                        <p>{post.name}</p>
-                        <img src={post.image} alt="random"/>
-                        <p>{post.description}</p>
-                        </div>
-                    })}
-        </div>
-        </div>
-        )}}
+                    <PostItem savedPosts={savedPosts} />
+                </div>
+            </div>
+        )
+    }
+}
 
-                
+
 
 export default Content
