d60aeb619e01d1174f49d41ec3b9fcce7c2d7999
Josh-Wickens <joshwickens@hotmail.com>
2022-12-19 21:27:22 +0000

    start challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,18 @@
 import css from './App.module.css';
-import Sidebar from "./components/Sidebar";
-// import NavBarSimple from "./components/NavBarSimple";
-import NavBarForm from "./components/NavBarForm";
-import Content from "./components/Content"
+// import Sidebar from "./components/Sidebar";
+// // import NavBarSimple from "./components/NavBarSimple";
+// import NavBarForm from "./components/NavBarForm";
+// import Content from "./components/Content"
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className={css.App}>
-      {/* <NavBarSimple /> */}
+      {/* <NavBarSimple /> 
       <NavBarForm />
       <Sidebar />
-      <Content />
+      <Content /> */}
+      <SearchBar />
     </div>
   );
 }
---
+++ my-app/src/components/SearchBar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import importedNames from '../names'
+
+export class SearchBar extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             importedNames: 'names'
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>Name Search</h1>
+                <form>
+                    <input
+                        type="text"
+                        placeholder="search names..."
+                    />
+                </form>
+                <div style={{margin: 'auto'}}>
+                    we will render names here
+                    
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SearchBar
