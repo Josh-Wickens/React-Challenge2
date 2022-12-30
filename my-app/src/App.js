e078f179cdec7020b93470b6cd084df4ff008414
Josh-Wickens <joshwickens@hotmail.com>
2022-12-30 17:34:07 +0000

    hooks challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,21 @@
 import css from './App.module.css';
-// import Sidebar from "./components/Sidebar";
+import Sidebar from "./components/Sidebar";
 // // import NavBarSimple from "./components/NavBarSimple";
-// import NavBarForm from "./components/NavBarForm";
-// import Content from "./components/Content"
-import SearchBar from './components/SearchBar';
+import NavBarForm from "./components/NavBarForm";
+import Content from "./components/Content"
+// import SearchBar from './components/SearchBar';
+import ContentHooks from './components/ContentHooks';
 
 function App() {
   return (
     <div className={css.App}>
       {/* <NavBarSimple /> 
-      <NavBarForm />
-      <Sidebar />*/}
+      
       <SearchBar />
-      {/* <Content />  */}
+      <Content />  */}
+      <NavBarForm />
+      <Sidebar />
+      <ContentHooks />
     </div>
   );
 }
